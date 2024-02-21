5e0347633d183fcb163f26c784ec61581341c6da
Zach Milo <zachmilo@gmail.com>
2024-02-20 19:14:37 -0500

    login error fixes

---
+++ react-app/src/components/SignupFormModal/copy.js
@@ -0,0 +1,62 @@
+{/* <div className="main-signup-div">
+
+<div className="signup-modal-container">
+	<h1 className="signup-title">Sign Up</h1>
+	<form className="signup-form" onSubmit={handleSubmit}>
+	<ul className="signup-errors">
+		{errors.map((error, idx) => (
+			<li className="signup-error" key={idx}>{error}</li>
+		))}
+	</ul>
+		<div className="signup-field">
+			<label className="signup-label">
+				Email
+				<input
+					className="signup-input"
+					type="text"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					required
+				/>
+			</label>
+		</div>
+		<div className="signup-field">
+			<label className="signup-label">
+				Username
+				<input
+					className="signup-input"
+					type="text"
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+					required
+				/>
+			</label>
+		</div>
+		<div className="signup-field">
+			<label className="signup-label">
+				Password
+				<input
+					className="signup-input"
+					type="password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					required
+				/>
+			</label>
+		</div>
+		<div className="signup-field">
+			<label className="signup-label">
+				Confirm Password
+				<input
+					className="signup-input"
+					type="password"
+					value={confirmPassword}
+					onChange={(e) => setConfirmPassword(e.target.value)}
+					required
+				/>
+			</label>
+		</div>
+		<button className="signup-submit-button" type="submit">Sign Up</button>
+	</form>
+</div>
+</div> */}
